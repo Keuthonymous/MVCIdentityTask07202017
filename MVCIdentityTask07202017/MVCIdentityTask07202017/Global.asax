e940bdea51d20b39cf0d764835987c92a36231a0
Mike Daughtrey <mikie.daughtrey@gmail.com>
2017-07-20 13:28:27 +0200

    Initial

---
+++ MVCIdentityTask07202017/MVCIdentityTask07202017/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCIdentityTask07202017.MvcApplication" Language="C#" %>
---
+++ MVCIdentityTask07202017/MVCIdentityTask07202017/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCIdentityTask07202017.Startup))]
+namespace MVCIdentityTask07202017
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
